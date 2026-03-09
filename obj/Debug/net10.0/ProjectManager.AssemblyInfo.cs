7e5466e1943d98a7e35057b6e3c1f503720158f1
Martin Harawa <martindharawa@gmail.com>
2026-03-09 17:15:39 +0200

    Updated the design for view proponent and view designs where now the tabs and the tabcontent are being treated as a single div and the styling of all the content inside the div has been shifted.

--- obj/Debug/net10.0/ProjectManager.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjectManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d75c14ff2f32c2acf8f256f971f4fcf00295a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c40152e9aacd00d69a5751a3c4ea2c41814d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

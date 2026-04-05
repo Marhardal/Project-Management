938c1341649be00f7589f443c60a78166358c70d
Martin Harawa <martindharawa@gmail.com>
2026-04-05 13:55:57 +0200

    Added new page for proposals. Added new styling for projects filters and added ne colums.

--- obj/Debug/net10.0/ProjectManager.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjectManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360fd6c46eb3d85945880a49a38ee23e97a757bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc55c5f5e9332a892139d38ad997dd2e8c0aacb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
